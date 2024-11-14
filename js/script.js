7af8ebcab873531cc876af7d8891ce2e0a6fbc36
Armando Interaminense <99498988+ainteraminense@users.noreply.github.com>
2024-11-14 17:28:14 -0500

    Write the logic to get the computer choice

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-console.log("Hello World");
+// CREATE function
+function getComputerChoice(){
+// GET randomly rock, paper or scissors
+let computerChoiceNumber = Math.random() * 3;
+switch (Math.floor(computerChoiceNumber) + 1) {
+    case 1:
+        return "rock";
+        break;
+    case 2:
+        return "paper";
+        break;
+    case 3:
+        return "scissors"
+        break;
+    }
+}
+// DISPLAY the values generated
+console.log(getComputerChoice());
