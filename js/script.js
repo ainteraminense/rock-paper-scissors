eee4e3abbcd6500d379ab2291e56e71d8fecbfaf
Armando Interaminense <99498988+ainteraminense@users.noreply.github.com>
2024-11-14 18:01:51 -0500

    Write the logic to get the human choice

--- js/script.js
+++ js/script.js
@@ -1,8 +1,37 @@
 // CREATE function
 function getComputerChoice(){
 // GET randomly rock, paper or scissors
-let computerChoiceNumber = Math.random() * 3;
-switch (Math.floor(computerChoiceNumber) + 1) {
+let computerChoiceNumber = Math.floor(Math.random() * 3) + 1 ;
+return translateNumberToChoice(computerChoiceNumber);
+}
+
+// DISPLAY the values generated
+console.log(getComputerChoice());
+
+//CREATE function
+function getHumanChoice() {
+
+    //GET the human choice for rock, paper or scissors
+    let promptChoiceNumber = () => prompt(`Please, select one of the options below:
+     1: rock
+     2: paper
+     3: scissors`);
+    let humanChoiceNumber = promptChoiceNumber();
+    //WHILE the human choice is not one of the options  
+    while(humanChoiceNumber != 1
+         && humanChoiceNumber != 2
+         && humanChoiceNumber != 3) {
+            humanChoiceNumber = promptChoiceNumber();
+         } 
+    return translateNumberToChoice(Number(humanChoiceNumber));
+}
+
+//DISPLAY human choice
+console.log(getHumanChoice());
+
+//REUSED function
+function translateNumberToChoice(number) {
+switch (number) {
     case 1:
         return "rock";
         break;
@@ -14,5 +43,3 @@ switch (Math.floor(computerChoiceNumber) + 1) {
         break;
     }
 }
-// DISPLAY the values generated
-console.log(getComputerChoice());
